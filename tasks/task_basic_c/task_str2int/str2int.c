cdca82842405fe204244546245d17837b2d9686b
DimaRed6 <krasnovdima0506@gmail.com>
2024-10-11 23:26:23 +0300

    Update str2int.c

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -3,5 +3,24 @@
 #include "stdio.h"
 
 int str2int(const char *str) {
-    return 0;
+   int znak =1;
+    int result = 0;
+    if (*str == '-'){
+        znak = -1;
+        str++;
+    } else if (*str == '+') {
+        str++;
+    }
+    
+    int count = 0;
+    while (*str != '\0') {
+        assert (result < 214748364 || 
+               (result == 214748364 && (*str - '0') < 8 && znak == 1) ||
+               (result == 214748364 && (*str - '0') < 9 && znak == -1));     
+        result = result * 10 + (*str - '0');
+        count += 1;
+        str++;
+    }
+   assert(count > 0);
+   return result * znak;
 }
