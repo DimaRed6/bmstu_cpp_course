123339cdde9074fc6625aabf058a725667725f48
DimaRed6 <krasnovdima0506@gmail.com>
2024-10-28 18:28:33 +0300

    Update int2str.c

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,44 @@
-#include <assert.h>
 #include "int2str.h"
-#include "stdio.h"
+
+#include <limits.h>
+#include <stdlib.h>
+
 
 char* int2str(int number) {
-    char* str = "0";
+    if (number == 0) {
+        return "0";
+    }
+
+    if (number == INT_MIN) {
+        return "-2147483648";
+    }
+
+    char* str = calloc(13, sizeof(char));
+    int len = 0;
+
+    if (number < 0) {
+        len++;
+        str[0] = '-';
+        number *= -1;
+    }
+
+    int copy = number;
+    while (copy > 0) {
+        copy /= 10;
+        len++;
+    }
+
+    int digit = 0;
+    int count = 0;
+    char ch = 0;
+    while (number > 0) {
+        digit = number % 10;
+        number = number / 10;
+        ch = digit + '0';
+        str[len - 1 - count] = ch;
+        count++;
+    }
+    str[len] = '\0';
+
     return str;
 }
