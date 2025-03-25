625432b32b2af01d1cf53a165d5fb463371d0308
DimaRed6 <krasnovdima0506@gmail.com>
2025-03-25 13:42:45 +0300

    basic_c

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,9 +1,39 @@
 #include "int2str.h"
-#include <assert.h>
-#include "stdio.h"
+#include <stdlib.h>
 
-char* int2str(int number)
-{
-	char* str = "0";
-	return str;
+
+char* int2str(int num) {
+    char * str = calloc(12, sizeof(char));
+    int len = 0;
+
+    unsigned int number = 0;
+    if (num < 0) {
+        len++;
+        str[0] = '-';
+        number = num * -1;
+    } else {
+        number = num;
+    }
+
+      int copy = number;
+    int count = 0;
+    while ((copy == 0 && count < 1) || copy > 0) {
+        copy /= 10;
+        len++;
+        count++;
+    }
+
+    int digit = 0;
+    count = 0;
+    char ch = 0;
+    while ((number == 0 && count < 1) || number > 0) {
+        digit = number % 10;
+        number = number / 10;
+        ch = digit + '0';
+        str[len - 1 - count] = ch;
+        count++;
+    }
+    str[len] = '\0';
+
+    return str;
 }
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,8 +1,25 @@
-#include "str2int.h"
 #include <assert.h>
+#include "str2int.h"
 #include "stdio.h"
 
-int str2int(const char* str)
-{
-	return 0;
+int str2int(const char *str) {
+    int m_sign = 1;
+    int result = 0;
+    if (*str == '-') {
+        m_sign = -1;
+        str++;
+    } else if (*str == '+') {
+        str++;
+    }
+    int m_count = 0;
+    while (*str != '\0') {
+        assert(result < 214748364 ||
+               (result == 214748364 && (*str - '0') < 8 && m_sign == 1) ||
+               (result == 214748364 && (*str - '0') < 9 && m_sign == -1));
+        result = result * 10 + (*str - '0');
+        m_count++;
+        str++;
+    }
+    assert(m_count > 0);
+    return result * m_sign;
 }
