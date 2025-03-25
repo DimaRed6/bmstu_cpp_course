9de199feb0071168a402adbe59975b368a3dac4d
DimaRed6 <krasnovdima0506@gmail.com>
2025-03-25 15:30:47 +0300

    basic_c

--- tasks/task_basic_c/task_int2str/int2str_tests.cpp
+++ tasks/task_basic_c/task_int2str/int2str_tests.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // NOLINTBEGIN
 #include <gtest/gtest.h>
 // NOLINTEND
@@ -26,27 +25,4 @@ TEST(int2str, BasicTestNumbers)
 	EXPECT_STREQ(int2str(-123), "-123");
 	EXPECT_STREQ(int2str(2147483647), "2147483647");
 	EXPECT_STREQ(int2str(-2147483648), "-2147483648");
-=======
-#include <gtest/gtest.h>
-#include "int2str.h"
-
-TEST(int2str, BasicTest) {
-    EXPECT_STREQ(int2str(0), "0");
-}
-
-TEST(int2str, BasicTestZero) {
-    EXPECT_STREQ(int2str(0), "0");
-}
-
-TEST(int2str, BasicTestZeroNext) {
-    EXPECT_STREQ(int2str(-0), "0");
-}
-
-TEST(int2str, BasicTestNumbers) {
-    EXPECT_STREQ(int2str(-5), "-5");
-    EXPECT_STREQ(int2str(-66), "-66");
-    EXPECT_STREQ(int2str(-123), "-123");
-    EXPECT_STREQ(int2str(2147483647), "2147483647");
-    EXPECT_STREQ(int2str(-2147483648), "-2147483648");
->>>>>>> d0aae5aa09ea330c5b7a4362ae24fa27e5604db8
 }
